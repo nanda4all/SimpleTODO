437bc52627e7ccd28028ac6a404fc78dc41df992
Prem Kumar Nanda <kumar.n@alsalam.local>
2022-05-30 18:59:38 +0300

    first commit

---
+++ src/app/MyComponents/add-todo/add-todo.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-add-todo',
+  templateUrl: './add-todo.component.html',
+  styleUrls: ['./add-todo.component.css']
+})
+export class AddTodoComponent implements OnInit {
+  title: string;
+  desc: string;
+  @Output() todoAdd: EventEmitter<Todo> = new EventEmitter();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onSubmit() {
+    if (this.title == null || this.desc == null) {
+      alert("Add Title and description");
+    }
+    else {
+      const todo = {
+        sno: 4,
+        title: this.title,
+        desc: this.desc,
+        active: true
+      }
+      this.todoAdd.emit(todo);
+    }
+  }
+}
